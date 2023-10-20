7acf2f161e4519342399b3bc234e144edbdf0f00
Md-Nuruzzaman-Nirob <info.mdnuruzzaman1@gmail.com>
2023-10-20 20:03:02 +0600

    update the banner section

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -9,17 +9,19 @@ const Banner = () => {
         backgroundPosition: "center",
         backgroundRepeat: " no-repeat",
       }}
-      className=" h-[100vh] "
+      className="h-[30vh] md:h-[55vh] lg:h-[65vh] xl:h-[75vh] 2xl:h-[85vh] "
     >
-      <div className="h-[100vh] text-white text-center pt-52">
-        <p className="font-Heebo text-xl text-gray-200">
+      <div className=" h-[30vh] md:h-[55vh] lg:h-[65vh] xl:h-[75vh] 2xl:h-[85vh]  text-white text-center pt-10 md:pt-16 xl:pt-20 2xl:pt-40">
+        <p className="font-Heebo text-[11px] md:text-sm lg:text-base xl:text-lg 2xl:text-xl text-gray-200 w-10/12 md:w-full mx-auto">
           Your next adventure starts here. Drive with confidence in our
           high-quality, well-maintained vehicles.
         </p>
-        <h3 className="font-Bebas text-8xl my-4">
+        <h3 className="font-Bebas text-2xl md:text-4xl lg:text-5xl xl:text-7xl 2xl:text-8xl my-2 lg:my-4">
           Quality vehicles for Every Journey
         </h3>
-        <button className="btn">Explore More</button>
+        <button className="btn btn-sm lg:btn-md rounded-md">
+          Explore More
+        </button>
       </div>
     </div>
   );
--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -1,9 +1,88 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import NavBar from "../../common/NavBar";
 import SocialLogin from "./SocialLogin";
 import Footer from "../../common/Footer";
+import useAuth from "../../../hooks/useAuth";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../../../firebase/firebase.config";
+import { PiWarningOctagonFill } from "react-icons/pi";
+import Swal from "sweetalert2";
 
 const Login = () => {
+  // use state
+  const [emailValidation, setEmailValidation] = useState("");
+  const [passwordValidation, setPasswordValidation] = useState("");
+
+  // context
+  const { loginUser } = useAuth();
+  // navigate
+  const navigate = useNavigate();
+  // use location
+  const location = useLocation();
+  // use ref
+  const emailRef = useRef();
+
+  const handleSignInUser = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(e);
+    console.log(form);
+
+    setEmailValidation("");
+    setEmailValidation("");
+
+    loginUser(email, password)
+      .then(() => {
+        toast.success("Login successfully.", {
+          position: "top-center",
+          duration: 4000,
+        });
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        if (
+          error.message === "Firebase: Error (auth/invalid-login-credentials)."
+        ) {
+          setPasswordValidation("Invalid password mismatch.");
+        } else if (
+          error.message ===
+          "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests)."
+        ) {
+          setPasswordValidation(
+            "Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. "
+          );
+        }
+        toast.error(error.message.slice(10));
+      });
+  };
+
+  // Forgot Password email verification
+  const handleForgetPassword = () => {
+    const email = emailRef.current.value;
+    if (email.length === 0) {
+      return setEmailValidation("Please provide an email address.");
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    ) {
+      return setEmailValidation("Please enter a valid email address.");
+    }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        Swal.fire({
+          title: "Please check yor email.",
+          icon: "question",
+          confirmButtonText: "OK",
+          showCloseButton: true,
+        });
+        setEmailValidation("");
+        setPasswordValidation("");
+      })
+      .catch(() => {});
+  };
   return (
     <>
       <NavBar></NavBar>
@@ -16,26 +95,53 @@ const Login = () => {
             <div className="flex-1 border-t border-gray-300"></div>
           </div>
 
-          <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-            <div className="mb-4 flex flex-col gap-6">
+          <form
+            onSubmit={handleSignInUser}
+            className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+          >
+            <div className="mb-4 flex flex-col gap-8">
               <div className="relative h-11 w-full min-w-[200px]">
                 <input
+                  ref={emailRef}
                   className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                   placeholder=" "
+                  name="email"
+                  required
                 />
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   Email
                 </label>
+                {emailValidation && (
+                  <p className="flex items-center gap-1 text-[#e83e8c] text-sm mt-1">
+                    <span>
+                      {" "}
+                      <PiWarningOctagonFill></PiWarningOctagonFill>
+                    </span>
+                    {emailValidation}
+                  </p>
+                )}
               </div>
+
               <div className="relative h-11 w-full min-w-[200px]">
                 <input
                   type="password"
                   className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                   placeholder=" "
+                  required
+                  name="password"
                 />
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   Password
                 </label>
+                {passwordValidation && (
+                  <p className="flex items-center gap-1 text-[#e83e8c] text-sm mt-1">
+                    <span>
+                      {" "}
+                      <PiWarningOctagonFill></PiWarningOctagonFill>
+                    </span>
+                    {passwordValidation}
+                  </p>
+                )}
               </div>
             </div>
             <div className="flex justify-between items-center">
@@ -76,13 +182,16 @@ const Login = () => {
                   </p>
                 </label>
               </div>
-              <a className="flex items-center text-sm leading-normal text-gray-700 antialiased font-medium hover:text-red-900 hover:underline transition-colors ml-1 font-Heebo">
+              <a
+                onClick={handleForgetPassword}
+                className="flex items-center text-sm leading-normal text-gray-700 antialiased font-medium hover:text-red-900 hover:underline transition-colors ml-1 font-Heebo"
+              >
                 forget password?
               </a>
             </div>
             <button
               className="btn mt-6 w-full border-none rounded-md bg-red-900 hover:bg-red-900 text-center font-Heebo text-lg font-bold uppercase text-white shadow-md transition-all hover:shadow-md hover:shadow-red-900/80 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              type="button"
+              type="submit"
               data-ripple-light="true"
             >
               Login
